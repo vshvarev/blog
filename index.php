1cdb98e64c885f97c5565f577116498d6400b377
shvarev <v.m.shvarev@yandex.ru>
2022-01-14 20:27:41 +0300

    Added Twig and templates

--- index.php
+++ index.php
@@ -3,17 +3,26 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 
 require __DIR__ . '/vendor/autoload.php';
 
+$loader = new FilesystemLoader('templates');
+$view = new Environment($loader);
+
 $app = AppFactory::create();
 
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
+$app->get('/', function (Request $request, Response $response, $args) use ($view) {
+    $body = $view->render('index.twig');
+    $response->getBody()->write($body);
     return $response;
 });
-$app->get('/about', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("About page");
+$app->get('/about', function (Request $request, Response $response, $args) use ($view) {
+    $body = $view->render('about.twig', [
+        'name' => 'Vitaliy'
+    ]);
+    $response->getBody()->write($body);
     return $response;
 });
 
